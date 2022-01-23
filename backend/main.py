9445e8db779fa4031c2562bf7bb927a8324f934f
gigajet <gigajet08092000@gmail.com>
2022-01-23 23:18:06 +0700

    update: process on numpy instead of file_path

--- backend/main.py
+++ backend/main.py
@@ -49,9 +49,7 @@ async def search_image(
     print("hello ", x1, x2, y1, y2)
     data = load_image_into_numpy_array(await image.read())
     data = data[y1:y2+1, x1:x2+1, :]
-    im = Image.fromarray(data)
-    im.save("your_file.png")
-    topK = process('your_file.png')
+    topK = process(data)
 
     print("np: ", data.shape)
     # return "image " + str(data.shape) \
--- process/model/main.py
+++ process/model/main.py
@@ -10,13 +10,13 @@ def cos_sim_2d(x, y):
     norm_y = y / np.linalg.norm(y, axis=1, keepdims=True)
     return np.matmul(norm_x, norm_y.T)
 
-def process(img_path):
+def process(img):
     collection_vector_path = "process/collection_vector/model1_vec.pickle"
     collection_path = "data/oxbuild_images/"
     with open(collection_vector_path, 'rb') as handle:
         collection_vec = pickle.load(handle)
         
-    img = cv2.imread(img_path)
+    # img = cv2.imread(img_path)
     model = Model(1)
 
     img = cv2.resize(img, (224, 224))
