7e12ca53792102f1c77272f45305826ab5f4d85e
zero1778 <bangdang2000@gmail.com>
2022-01-23 23:45:52 +0700

    fix rgba and grayscale

--- process/model/main.py
+++ process/model/main.py
@@ -1,15 +1,9 @@
 
 from json.tool import main
 import cv2, os
-<<<<<<< HEAD
-from PIL import Image
-from model import Model
-import torch
-import pickle5 as pickle
-=======
 from process.model.model import Model
 import torch, pickle
->>>>>>> 9445e8db779fa4031c2562bf7bb927a8324f934f
+import pickle5 as pickle
 import numpy as np
 
 def cos_sim_2d(x, y):
@@ -23,18 +17,12 @@ def process(img):
     with open(collection_vector_path, 'rb') as handle:
         collection_vec = pickle.load(handle)
         
-<<<<<<< HEAD
-    # Opens a image in RGB mode
-    # img = Image.open('../data/oxbuild_images/all_souls_000000.jpg')    
-    # import pdb; pdb.set_trace()
-    r,g,b = cv2.split(img)
-    img_bgr = cv2.merge([b,g,r])
-    
-    img = cv2.imread('../data/oxbuild_images/all_souls_000000.jpg')
-=======
-    # img = cv2.imread(img_path)
->>>>>>> 9445e8db779fa4031c2562bf7bb927a8324f934f
     model = Model(1)
+
+    if (img.shape[-1] > 3):
+        img = img[:, :, :3]
+    elif (img.shape[2] == None):
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
 
     img = cv2.resize(img, (224, 224))
     img = img.transpose((2, 0, 1))
@@ -49,7 +37,6 @@ def process(img):
     imgs = sorted(os.listdir(collection_path))
     priority_list = [imgs[i] for i in priority_list_idx]
 
-    # print(priority_list)
     return priority_list
 
 
