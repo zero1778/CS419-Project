632e995aff4bd53453c878efcd91cf10f1fc7a48
gigajet <gigajet08092000@gmail.com>
2022-01-24 23:13:53 +0700

    fe fix: use result image instead of showcase

--- frontend/gallery.js
+++ frontend/gallery.js
@@ -1,7 +1,7 @@
 
 window.onload = function initializeResultGallery() {
-    let data = ['all_souls_000006.jpg', 'all_souls_000003.jpg', 'all_souls_000010.jpg', 'all_souls_000008.jpg', 'all_souls_000005.jpg', 'all_souls_000001.jpg', 'all_souls_000011.jpg', 'all_souls_000007.jpg', 'all_souls_000002.jpg', 'all_souls_000000.jpg']
-    //let data = JSON.parse(sessionStorage.getItem('feir_result_data'))
+    //let data = ['all_souls_000006.jpg', 'all_souls_000003.jpg', 'all_souls_000010.jpg', 'all_souls_000008.jpg', 'all_souls_000005.jpg', 'all_souls_000001.jpg', 'all_souls_000011.jpg', 'all_souls_000007.jpg', 'all_souls_000002.jpg', 'all_souls_000000.jpg']
+    let data = JSON.parse(sessionStorage.getItem('feir_result_data'))
     sessionStorage.removeItem('feir_result_data')
     let queryImg = JSON.parse(sessionStorage.getItem('feir_query_image')) 
     if (queryImg) {
