79ac6366c336f3a92498a758536e4da5194265bc
bxbao87 <bxbao18@apcs.vn>
2022-01-28 00:25:09 +0700

    [process][update] build_dict and main

--- process/build_dict.py
+++ process/build_dict.py
@@ -8,24 +8,32 @@ def main():
     collection_path = "../data/oxbuild_images/"
     collection_vector_path = "./collection_vector/"
     imgs = sorted(os.listdir(collection_path))
-    result = []
-    model = Model(1)
-    for each in tqdm(imgs):
-        img_path = collection_path + each
-        img = cv2.imread(img_path) 
-        img = cv2.resize(img, (224, 224))
-        img = img.transpose((2, 0, 1))
-        img = img.reshape((1, 3, 224, 224))
-        img = torch.from_numpy(img)
-        img = img.float()
-        output = model.predict(img).reshape(-1)
-        result.append(output)
 
-    result = torch.stack(result).detach().numpy()
- 
-    with open(collection_vector_path + 'model1_vec.pickle', 'wb') as handle:
-        pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    image_collection = [imgs[x:x+900] for x in range(0, len(imgs), 900)]
+    
+    model = Model(1)    
 
+    i = 1
+    for chuck in image_collection:
+        result = []
+        for each in tqdm(chuck):
+            img_path = collection_path + each
+            img = cv2.imread(img_path) 
+            img = cv2.resize(img, (224, 224))
+            img = img.transpose((2, 0, 1))
+            img = img.reshape((1, 3, 224, 224))
+            img = torch.from_numpy(img)
+            img = img.float()
+            output = model.predict(img).reshape(-1)
+            result.append(output)
+
+        result = torch.stack(result).detach().numpy()
+    
+        with open(collection_vector_path + 'model1_vec_' + str(i) + '.pickle', 'wb') as handle:
+            pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        i+=1
+        break
 
 if __name__ == "__main__":
     main()
--- process/model/main.py
+++ process/model/main.py
@@ -6,12 +6,15 @@ import torch, pickle
 # import pickle5 as pickle
 import numpy as np
 
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
 def cos_sim_2d(x, y):
     norm_x = x / np.linalg.norm(x, axis=1, keepdims=True)
     norm_y = y / np.linalg.norm(y, axis=1, keepdims=True)
     return np.matmul(norm_x, norm_y.T)
 
-def process(img):
+def process(img, topK=20):
     collection_vector_path = "process/collection_vector/model1_vec.pickle"
     collection_path = "data/oxbuild_images/"
     with open(collection_vector_path, 'rb') as handle:
@@ -32,8 +35,7 @@ def process(img):
     output = model.predict(img).reshape(-1).reshape(1, -1).detach().numpy()
 
     cosine_similarity = cos_sim_2d(output, collection_vec).reshape(-1)
-    priority_list_idx = cosine_similarity.argsort()[(-1*collection_vec.shape[0]):][::-1]
-
+    priority_list_idx = cosine_similarity.argsort()[-topK:][::-1]
     imgs = sorted(os.listdir(collection_path))
     priority_list = [imgs[i] for i in priority_list_idx]
 
