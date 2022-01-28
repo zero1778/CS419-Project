b84d2277680a2f5595c69d8415bff71566cc2a6c
zero1778 <bangdang2000@gmail.com>
2022-01-28 23:46:27 +0700

    add image processing to evaluation

--- eval.py
+++ eval.py
@@ -16,15 +16,15 @@ queries = [q[:-end] for q in files if q.endswith("query.txt")]
 
 for q in queries:
     gtq = path + q 
-    with open(gtq, 'r') as f:
+    with open(gtq + "_query.txt", 'r') as f:
         lines = f.readlines()
         for line in lines:
             line = line.split(' ')
             img_name = line[0][5:]
             img_name = img_name + '.jpg'
-    img = cv2.read(img_path + img_name)
+    img = cv2.imread(img_path + img_name)
     topK = process(img)
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     resultPath = rPath + q + "_top.txt"
     with open(resultPath, 'w') as f:
         f.write("\n".join(topK))
