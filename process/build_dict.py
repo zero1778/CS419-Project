1e545fe5e2a8f56d652ffbdc413e5f7a72a6901d
gigajet <gigajet08092000@gmail.com>
2022-02-04 08:32:16 +0700

    upd model3 code: sift of downsample 4 + knn matchr

--- process/build_dict.py
+++ process/build_dict.py
@@ -3,18 +3,17 @@ from model.model import Model
 from model.utils import norm_mean_std
 from tqdm import tqdm
 import torch, pickle
-from model.utils import norm_mean_std
 
 
 def main():
     collection_path = "./data/oxbuild_images/"
-    collection_vector_path = "./process/collection_vector/model3/"
+    collection_vector_path = "./process/collection_vector/"
     imgs = sorted(os.listdir(collection_path))
+    image_collection = [imgs[x:x+900] for x in range(0, len(imgs), 900)]
     # print(imgs[:10])
     # image_collection = [imgs[:]]
-    image_collection = [imgs[x:min(x+900, len(imgs))] for x in range(0, len(imgs), 900)]
     
-    model = Model(3)    
+    model = Model(2)    
 
     i = 1
     for chuck in image_collection:
@@ -25,13 +24,14 @@ def main():
             img = cv2.resize(img, (224, 224))
             img = norm_mean_std(img)
             img = torch.from_numpy(img)
+
+            # img = img.float()
             output = model.predict(img).reshape(-1)
-            # import pdb;pdb.set_trace()
             result.append(output)
 
         result = torch.stack(result).detach().numpy()
     
-        with open(collection_vector_path + 'model3_vec_' + str(i) + '.pickle', 'wb') as handle:
+        with open(collection_vector_path + 'model1_vec_' + str(i) + '.pickle', 'wb') as handle:
             pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
         i+=1
